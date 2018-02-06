9502b6d8b04beb20efbb8109e2002ea5130ba8e4
Alex Andrade <g.alex.andrade@gmail.com>
2018-02-06 18:00:45 -0200

    Lombok implementation

--- src/main/java/com/recipe/app/domain/Category.java
+++ src/main/java/com/recipe/app/domain/Category.java
@@ -1,8 +1,11 @@
 package com.recipe.app.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
 @Entity
 public class Category {
     @Id
@@ -13,27 +16,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
